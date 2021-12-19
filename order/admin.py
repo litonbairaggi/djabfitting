fcfed7c56fed8c559e52b72b38a25d0c5a6dbbe8
litonbairaggi <litonovi2013@gmail.com>
2021-12-20 02:32:11 +0600

    add admin models

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import register
+from .models import Order
 
 # Register your models here.
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['name', 'mobile', 'category', 'design', 'description', 'delivery', 'price', 'payment', 'received_date', 'due']
