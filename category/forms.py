4590b8d85d63213ba3a54ebb6765dcf297494b5f
litonbairaggi <litonovi2013@gmail.com>
2021-12-21 13:44:34 +0600

    commit from my computer

--- category/forms.py
+++ category/forms.py
@@ -4,7 +4,8 @@ from django.forms import fields, widgets
 from .models import Category
 
 class CategoryForm(forms.ModelForm):
-    category_name = forms.CharField(label="Category pppplllll Name adadaaa", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Category Name'}), required=True, error_messages={'required':'Must Enter a Currect Name'})
+    category_name = forms.CharField(label='Category Name', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Category Name'}), required=True, error_messages={'required':'Must Enter a Correct Name'})
     class Meta:
         model = Category
         fields = '__all__'
+        # fields = ['id', 'category_name']
